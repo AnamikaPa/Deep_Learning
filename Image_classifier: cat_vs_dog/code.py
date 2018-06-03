25e0f153893816169a2b11594241498267b22aeb
Anamika Patel <anamikap24@gmail.com>
2018-06-03 20:41:58 +0530

    Add files via upload

---
+++ Image_classifier: cat_vs_dog/code.py
@@ -0,0 +1,80 @@
+"""
+Download training data from https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/data
+and put that into "data" folder 
+"""
+
+import cv2
+import numpy as np 
+import os
+from random import shuffle
+from tqdm import tqdm
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+import tensorflow as tf
+
+
+TRAIN_DIR = "data"
+IMG_SIZE = 50
+LR = 1e-3 
+
+def label_img(img):
+    word_label = img.split('.')[-3]
+    if word_label == 'cat': return [1,0]
+    elif word_label == 'dog': return [0,1]
+
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    return training_data
+
+train_data = create_train_data()
+
+
+tf.reset_default_graph()
+
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 128, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+
+train = train_data[:-500]
+test = train_data[-500:]
+
+X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y = [i[1] for i in train]
+
+test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_y = [i[1] for i in test]
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=10, validation_set=({'input': test_x}, {'targets': test_y}), 
+    snapshot_step=500, show_metric=True)
+
